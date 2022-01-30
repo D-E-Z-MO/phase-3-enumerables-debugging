de784b092d3ab260a8445b4954cabd58e2a5f74a
douglassmoss <douglassmoss@icloud.com>
2022-01-30 12:52:27 -0800

    commit tree cee9c286a541ef289ae08aea0f4766c10589b644

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,11 @@
-# Write your code here
+require 'pry'
+
+# Split the string on "" to get access to each word in the sentence
+# Reverse each word , and add it to the new array (create new array, iterate, reverse & push)
+# Join the array of words on "" to create one string
+
+def reverse_each_word(sentence)
+  sentence.split.map(&:reverse).join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
